42e8f6e04f186787d08c75239ac2c67a5c11114c
Amir Hossein Baghernezhad <amhoba2014@gmail.com>
2024-11-26 22:52:58 +0330

    basic sdk usage

--- frontend/assets/app/page.tsx
+++ frontend/assets/app/page.tsx
@@ -1,7 +1,19 @@
+'use client';
+
+import * as sdk from '../sdk/sdk.gen'
+
+sdk.client.setConfig({
+  baseUrl: "/api"
+})
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       Just a test.
+      <button onClick={async () => {
+        const request = await sdk.readRootGet()
+        console.log(request.data)
+      }}>kjkjhj</button>
     </main>
   );
 }
