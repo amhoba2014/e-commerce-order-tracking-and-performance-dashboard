50a35296974f0ed634f81aa316950d1b19010ca5
Amir Hossein Baghernezhad <amhoba2014@gmail.com>
2024-12-19 19:19:31 +0330

    added code to spin up fakers

--- backend/assets/main.py
+++ backend/assets/main.py
@@ -1,12 +1,12 @@
 import asyncio
-import httpx
 from typing import List
-from fastapi import FastAPI, Depends, BackgroundTasks
+from fastapi import FastAPI, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from faker import Faker
 import random
 from datetime import datetime
+from source.makefake import spin_up_fakers
 from source.models import Order
 from source.database import create_db_and_tables, get_db
 from source.enums import OrderStatus, PaymentStatus
@@ -20,21 +20,10 @@ app.openapi = make_custom_openapi(app)
 faker = Faker()  # Initialize the Faker instance
 
 
-async def create_random_orders():
-  async with httpx.AsyncClient() as client:
-    while True:
-      try:
-        response = await client.post("http://localhost:8000/orders/random")
-        print(f"Created random order: {response.status_code}")
-      except Exception as e:
-        print(f"Error creating random order: {e}")
-      await asyncio.sleep(2)
-
-
 @app.on_event("startup")
 async def on_startup():
   await create_db_and_tables()
-  asyncio.create_task(create_random_orders())
+  await spin_up_fakers()
 
 
 @app.get("/orders/", response_model=List[Order])
---
+++ backend/assets/source/makefake.py
@@ -0,0 +1,17 @@
+import httpx
+import asyncio
+
+
+async def spin_up_fakers():
+  asyncio.create_task(create_random_orders())
+
+
+async def create_random_orders():
+  async with httpx.AsyncClient() as client:
+    while True:
+      try:
+        response = await client.post("http://localhost:8000/orders/random")
+        print(f"Created random order: {response.status_code}")
+      except Exception as e:
+        print(f"Error creating random order: {e}")
+      await asyncio.sleep(2)
