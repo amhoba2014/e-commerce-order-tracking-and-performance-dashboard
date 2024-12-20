12871ae27cdfe648393f400127f0c2eb5477c9ea
Amir Hossein Baghernezhad <amhoba2014@gmail.com>
2024-12-20 11:57:15 +0330

    just disabled the order creation

--- backend/assets/source/makefake.py
+++ backend/assets/source/makefake.py
@@ -7,11 +7,12 @@ async def spin_up_fakers():
 
 
 async def create_random_orders():
-  async with httpx.AsyncClient() as client:
-    while True:
-      try:
-        response = await client.post("http://localhost:8000/orders/random")
-        print(f"Created random order: {response.status_code}")
-      except Exception as e:
-        print(f"Error creating random order: {e}")
-      await asyncio.sleep(2)
+  pass
+  # async with httpx.AsyncClient() as client:
+  #   while True:
+  #     try:
+  #       response = await client.post("http://localhost:8000/orders/random")
+  #       print(f"Created random order: {response.status_code}")
+  #     except Exception as e:
+  #       print(f"Error creating random order: {e}")
+  #     await asyncio.sleep(2)
