4b8759262d4cdf145e42973f877841fd6e0e37a5
Amir Hossein Baghernezhad <amhoba2014@gmail.com>
2024-12-26 12:43:08 +0330

    updated to add foreign keys

--- backend/assets/source/models.py
+++ backend/assets/source/models.py
@@ -1,42 +1,9 @@
-from typing import Optional
-from sqlmodel import Field, SQLModel
-from sqlalchemy import Column, DateTime
+from typing import Optional, List
+from sqlmodel import Field, SQLModel, Relationship
+from sqlalchemy import Column, DateTime, ForeignKey
 from sqlalchemy.sql import func
-from datetime import datetime, timezone
+from datetime import datetime
 from source.enums import OrderStatus, PaymentStatus
-
-
-class Order(SQLModel, table=True):
-  orderId: str = Field(primary_key=True)
-  productId: str
-  customerId: str
-  status: OrderStatus = OrderStatus.Pending
-  quantity: float
-  paymentStatus: PaymentStatus = PaymentStatus.Pending
-  created: Optional[datetime] = Field(
-      sa_column=Column(DateTime(timezone=True), server_default=func.now())
-  )
-  updated: Optional[datetime] = Field(
-      sa_column=Column(DateTime(timezone=True),
-                       server_default=func.now(), onupdate=func.now())
-  )
-  deleted: bool = False
-
-
-class Customer(SQLModel, table=True):
-  customerId: str = Field(primary_key=True)
-  name: str
-  email: str
-  password: str
-  address: str
-  created: Optional[datetime] = Field(
-      sa_column=Column(DateTime(timezone=True), server_default=func.now())
-  )
-  updated: Optional[datetime] = Field(
-      sa_column=Column(DateTime(timezone=True),
-                       server_default=func.now(), onupdate=func.now())
-  )
-  deleted: bool = False
 
 
 class Product(SQLModel, table=True):
@@ -53,3 +20,40 @@ class Product(SQLModel, table=True):
                        server_default=func.now(), onupdate=func.now())
   )
   deleted: bool = False
+  orders: List["Order"] = Relationship(back_populates="product")
+
+
+class Customer(SQLModel, table=True):
+  customerId: str = Field(primary_key=True)
+  name: str
+  email: str
+  password: str
+  address: str
+  created: Optional[datetime] = Field(
+      sa_column=Column(DateTime(timezone=True), server_default=func.now())
+  )
+  updated: Optional[datetime] = Field(
+      sa_column=Column(DateTime(timezone=True),
+                       server_default=func.now(), onupdate=func.now())
+  )
+  deleted: bool = False
+  orders: List["Order"] = Relationship(back_populates="customer")
+
+
+class Order(SQLModel, table=True):
+  orderId: str = Field(primary_key=True)
+  productId: str = Field(foreign_key="product.productId")
+  customerId: str = Field(foreign_key="customer.customerId")
+  status: OrderStatus = OrderStatus.Pending
+  quantity: float
+  paymentStatus: PaymentStatus = PaymentStatus.Pending
+  created: Optional[datetime] = Field(
+      sa_column=Column(DateTime(timezone=True), server_default=func.now())
+  )
+  updated: Optional[datetime] = Field(
+      sa_column=Column(DateTime(timezone=True),
+                       server_default=func.now(), onupdate=func.now())
+  )
+  deleted: bool = False
+  product: Product = Relationship(back_populates="orders")
+  customer: Customer = Relationship(back_populates="orders")
